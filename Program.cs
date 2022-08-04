74e50cfad0ec04bc6c1ab5dc4602db775d76e1e0
Nitesh Sharma <niteshsharma9199@gmail.com>
2022-08-04 19:43:40 +0530

    Second Index source file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//how to find the second index of a character (this solution finds the second index of '-'). C# code
+using System;
+class Program
+{
+    public static void Main()
+    {
+        var str = "Hi-this-solution-gives-second-index-of-a-character";
+
+        Console.WriteLine("Index of second '-' is: "+SecondIndexOf(str));
+
+    }
+    public static int SecondIndexOf(string str)
+    {
+        return str.IndexOf('-', str.IndexOf('-') + 1);
+    }
+}
